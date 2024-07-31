0cac9f95ce647bd3e8fa843cad76e986874ef5e3
Sandra Maysonnave <sandra@MacBook-Air-de-Sandra.local>
2024-07-31 17:12:29 +0200

    Router+quelques vues

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,42 +1,57 @@
 <!-- Header template -->
 
 <template>
-    <div class="header_main">
-       <div class="container_head">
-          
-          <div class="logo"><img src="../assets/images/YBP_head_logo.png"></div>
-          <hr class="solid" style="width: 500px;">
+   <div class="header_main">
+      <div class="container_head">
 
-             <div class="menu_main">
-                <ul>
-                   <li><a href="index.html">Discover</a></li>
-                   <li class="dropdown">
-                      <a href="library.html">Library</a>
-                      <ul class="dropdown-menu">
-                         <li><a href="futuristic.html"><img src="../assets/images/Futuristic.png" alt="Futuristic"></a></li>
-                         <li><a href="medieval.html"><img src="../assets/images/Medieval.png" alt="Medieval"></a></li>
-                         <li><a href="action-scenes.html"><img src="../assets/images/Action scenes.png" alt="Action Scenes"></a></li>
-                         <li><a href="emotional.html"><img src="../assets/images/Emotional.png" alt="Emotional"></a></li>
-                         <li><a href="funny.html"><img src="../assets/images/Funny.png" alt="Funny"></a></li>
-                         <li><a href="romantic.html"><img src="../assets/images/Romantic.png" alt="Romantic"></a></li>
-                      </ul>
-                   </li>
-                   <li><a href="custom_music.html">Custom music</a></li>
-                   <li class="dropdown">
-                      <a href="about.html">The studios</a>
-                      <ul class="dropdown-menu">
-                         <li><a href="#">About us</a></li>
-                      </ul>
-                   </li>
-                   <li><a href="contact.html">Contact us</a></li>
-                </ul>
-             </div>
-       </div>
-    </div>
+         <div class="logo"><img src="../assets/images/YBP_head_logo.png"></div>
+         <hr class="solid" style="width: 500px;">
+
+         <div class="menu_main">
+            <ul>
+               <li>
+                  <router-link :to="{ name: 'home' }">
+                     Discover
+                  </router-link>
+               </li>
+               <li class="dropdown">
+                  <a href="library.html">Library</a>
+                  <ul class="dropdown-menu">
+                     <li><a href="futuristic.html"><img src="../assets/images/Futuristic.png" alt="Futuristic"></a></li>
+                     <li><a href="medieval.html"><img src="../assets/images/Medieval.png" alt="Medieval"></a></li>
+                     <li><a href="action-scenes.html"><img src="../assets/images/Action scenes.png"
+                              alt="Action Scenes"></a></li>
+                     <li><a href="emotional.html"><img src="../assets/images/Emotional.png" alt="Emotional"></a></li>
+                     <li><a href="funny.html"><img src="../assets/images/Funny.png" alt="Funny"></a></li>
+                     <li><a href="romantic.html"><img src="../assets/images/Romantic.png" alt="Romantic"></a></li>
+                  </ul>
+               </li>
+               <li>
+                  <router-link :to="{ name: 'custom_music' }">
+                     Custom music
+                  </router-link>
+               </li>
+               <li class="dropdown">
+                  <router-link :to="{ name: 'about' }">
+                     The studios
+                  </router-link>
+                  <ul class="dropdown-menu">
+                     <li><a href="#">About us</a></li>
+                  </ul>
+               </li>
+               <li>
+                  <router-link :to="{ name: 'contact' }">
+                     Contact us
+                  </router-link>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </div>
 </template>
 
 <script>
 export default {
-    name: "Header",
+   name: "Header",
 };
 </script>
---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Header from '@/components/Header.vue';
+</script>
+
+<template>
+    <div class="page"><img class="bg_image" src="../assets/images/bg1.jpeg">
+        <Header></Header>
+        <!-- about section start -->
+      <div class="about_section layout_padding">
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="about_taital_main">
+                     <h1 class="about_taital">About Us</h1>
+                     <p class="about_text">Hey, we're two crazy young french composers who want to reach success !</p>
+                     <div class="readmore_bt"><a href="#">Read More</a></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- about section end -->
+    </div>
+</template>

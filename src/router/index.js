63b47354cab936288df9ce43282b277a8f0a5e7c
Sandra Maysonnave <sandra@MacBook-Air-de-Sandra.local>
2024-08-20 23:18:54 +0200

    Scroll en haut de page quand on change de page

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import CustomMusic from '@/views/CustomMusic.vue'
 import Contact from '@/views/Contact.vue'
 import About from '@/views/About.vue'
 import Home from '@/views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
       path: '/custom_music',
       name: 'custom_music',
-      component: CustomMusic
+      component: CustomMusic,
     },
     {
       path: '/contact',
       name: 'contact',
-      component: Contact
-    },    {
+      component: Contact,
+    }, {
       path: '/about',
       name: 'about',
-      component: About
+      component: About,
     }
-  ]
+  ],
+  scrollBehavior() { document.getElementById('header').scrollIntoView(); },
 })
+
 
 export default router

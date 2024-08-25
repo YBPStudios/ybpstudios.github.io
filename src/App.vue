eda9e80d7f222638417e9e06e1081d0dd4b287aa
ThomasPeyrounette <88450894+ThomasPeyrounette@users.noreply.github.com>
2024-08-25 21:49:42 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
+   <head>
+      <!-- GENERAL INFO -->
+       <meta charset="utf-8">
+       <meta name="YellowBuoyProduction Studios" content="YBP Studios is a french music studio specialized for media Soundtracks (films, animation film, documentaries, video games, ads...). Unique story, unique music.">
+       <link rel="shortcut icon" type="image/x-icon" href="./assets/images/favicon.ico">
+   </head>
    <!-- Page loading -->
    <!-- <div id="loading-overlay">
          <video id="loading-video" src="videos/V3_animation.MOV" muted></video>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ybpstudios.github.io/',
   plugins: [
     vue(),
   ],
@@ -14,3 +15,4 @@ export default defineConfig({
     }
   }
 })
+

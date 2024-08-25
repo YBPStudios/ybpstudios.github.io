11d59c30315d31e72436387628d3aa9899c39a84
ThomasPeyrounette <88450894+ThomasPeyrounette@users.noreply.github.com>
2024-08-25 22:44:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'ybpstudios.com',
+  base: '/ybpstudios.github.io/',
   plugins: [
     vue(),
   ],

761733f2d4bb4b5f8bfd776cd8b31758b8866b58
ThomasPeyrounette <88450894+ThomasPeyrounette@users.noreply.github.com>
2024-08-31 15:28:32 +0200

    Website icon update

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,7 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-   <head>
-      <!-- GENERAL INFO -->
-       <meta charset="utf-8">
-       <meta name="description" content="YBP Studios is a French music studio specialized in media soundtracks (films, animation films, documentaries, video games, ads). Unique story, unique music.">
-       <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico" sizes="16x16">
-   </head>
+   
    <!-- Page loading -->
    <!-- <div id="loading-overlay">
          <video id="loading-video" src="videos/V3_animation.MOV" muted></video>
